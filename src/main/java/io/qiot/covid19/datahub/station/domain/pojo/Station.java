46c6524ac8d1381f9d8eacde8c6429b90155f36d
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-02 10:45:04 +0100

    switched to postgresql

--- src/main/java/io/qiot/covid19/datahub/station/domain/pojo/Station.java
+++ src/main/java/io/qiot/covid19/datahub/station/domain/pojo/Station.java
@@ -9,23 +9,30 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @Entity
 @Cacheable
-@Table(name = "station")
+@Table(name = "station", indexes = {//
+        @Index(name="station_serial_idx", columnList = "serial", unique = true),//
+        @Index(name="station_location_idx", columnList = "country_code, city", unique = true),//
+        @Index(name="station_coordinates_idx", columnList = "longitude, latitude", unique = true)//
+        })
 @RegisterForReflection
 public class Station extends PanacheEntityBase {
+    
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @GenericGenerator(name = "uuid-gen", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid-gen")
+    @Type(type="pg-uuid")
     public UUID id;
     @Column(nullable = false, unique = true)
     public String serial;
