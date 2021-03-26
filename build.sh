a85576822460452392a541d3bf1acc1ffef58b40
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-26 11:17:20 +0100

    updated build script and readme

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 chmod +x mvn*
 mvn -N io.takari:maven:wrapper
 ./mvnw clean package -Pnative  -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-datahub-station:1.0.0-alpha
-docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-station:1.0.0-alpha .
-docker push quay.io/qiotcovid19/qiot-datahub-station:1.0.0-alpha
+docker rmi quay.io/qiotcovid19/qiot-datahub-station:1.0.0-RC
+docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-datahub-station:1.0.0-RC .
+docker push quay.io/qiotcovid19/qiot-datahub-station:1.0.0-RC
 #docker run -it --rm -p 5000:5000 --net host quay.io/qiot/qiot-datahub-station
